1a0d81bca0a840e9d3764c79bff553a6ef5d9445
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-10-10 12:09:47 -0300

    veveq

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'geografia',
+    'qual o país conhecido como terra do sol nascente',
+    'resposta: ',
+
+    'geografia',
+    'qual é a capital do acre',
+    'resposta:',
+
+    'geografia',
+    'a qual continente pertence o Brasil',
+    'resposta:',
+
+    'portugues',
+    'o que significa onipresente'
+    'resposta:'
+
+    'portugues'
+    'o que é paroxitona'
+    'resposta:'
+    
+
+)
