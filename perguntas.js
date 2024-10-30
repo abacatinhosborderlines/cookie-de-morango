749fdb9740f89fec35a903c2ab3c85c221fbdd99
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-10-30 07:31:17 -0300

    asd

--- perguntas.js
+++ perguntas.js
@@ -1,104 +1,104 @@
 criaCartao(
-    'geografia',
-    'qual o país conhecido como terra do sol nascente',
-    'resposta: Japão ',)
+    'Geografia',
+    'Qual o país conhecido como terra do sol nascente',
+    'Resposta: Japão ',)
 
     criaCartao(
-    'geografia',
-    'qual é a capital do acre',
-    'resposta: Rio Branco',)
+    'Geografia',
+    'Qual é a capital do acre',
+    'Resposta: Rio Branco',)
 
     criaCartao(
-    'geografia',
-    'a qual continente pertence o Brasil',
-    'resposta: America do Sul',)
+    'Geografia',
+    'A qual continente pertence o Brasil',
+    'Resposta: America do Sul',)
 
     criaCartao(
-    'portugues',
-    'o que significa onipresente',
-    'resposta: Está em todos os lugares',)
+    'Portugues',
+    'O que significa onipresente',
+    'Resposta: Está em todos os lugares',)
 
     criaCartao(
-    'portugues',
-    'o que é paroxitona',
-    'resposta: Palavras que tem a penultima silaba tonica',)
+    'Portugues',
+    'O que é paroxitona',
+    'Resposta: Palavras que tem a penultima silaba tonica',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Quando começa a Primeira Guerra Mundial?',
     'Resposta: 1914',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Qual é a civilização mais antiga do mundo? ',
     'Resposta: Mesopotâmia',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Quem se chama Napoleão do Irã?', 
     'Resposta: Nader Shah',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Qual é a última dinastia na China?', 
     'Resposta: Dinastia Qing',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Quem é o primeiro presidente dos Estados Unidos?',
-    ' Resposta: Washington ',)
+    'Resposta: Washington ',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Em que ano John F. Kennedy foi assassinado?',
     'Resposta: 1963',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Qual presidente dos EUA tinha uma casa chamada The Hermitage?',
      'Resposta: Andrew Jackson',)
 
      criaCartao(
-     'historia',
+     'Historia',
     'Qual período foi conhecido como a Idade de Ouro de Roma? ',
     'Resposta: Augusto César',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Onde é realizada a primeira Olimpíada de Verão? ',
     'Resposta: Atenas, Grécia 1896',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Qual é a dinastia mais antiga ainda governando?',
      'Resposta:Japão',)
 
      criaCartao(
-     'historia',
+     'Historia',
     'A civilização asteca se originou de qual país?',
      'Resposta: México',)
 
      criaCartao(
-     'historia',
+     'Historia',
     'Quem estava entre os famosos poetas romanos?', 
      'Resposta: Virgílio',)
 
      criaCartao(
-     'historia',
+     'Historia',
     'Quem foi o primeiro americano a ganhar um Nobel da Paz?',
      'Resposta: Theodore Roosevelt',)
 
      criaCartao(
-'historia',
+'Historia',
      'Onde a Babilônia permanece localizada?',
     'Resposta: Iraquec?',)
    
     criaCartao(
-    'historia',
+    'Historia',
      'Quando Joana dArc foi beatificada na famosa catedral de Notre Dame em Paris?',
     'Resposta: 1909',)
 
     criaCartao(
-    'historia',
+    'Historia',
     'Quem foi o primeiro homem a pisar na lua?', 
     'Resposta: Neil Armstrong 1969',)
