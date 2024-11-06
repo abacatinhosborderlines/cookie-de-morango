197c91ae1db346fa4810e9320a21dd95e1033b06
abacatinhosborderlines <161480073+abacatinhosborderlines@users.noreply.github.com>
2024-11-06 08:42:08 -0300

    kk

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ function criaCartao (categoria,pergunta,resposta){
     cartao.className= 'cartao';
     cartao.innerHTML = `
     
-    <div class="conteudo-Cartao">
+    <div class="conteudo-cartao">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
     </div>
-    <div class="resposta-Cartao">
+    <div class="resposta-cartao">
         <p>${resposta}</p>
     </div>
 </div>
